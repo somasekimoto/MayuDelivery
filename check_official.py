33a0aede59acf48d87156c6b9f17989166b33adb
SomaSekimoto <0421.soma@gmail.com>
2020-10-15 22:48:19 +0900

    add: official icon image in Flex message

--- check_official.py
+++ check_official.py
@@ -17,20 +17,72 @@ access_token = os.getenv('TWITTER_ACCESS_TOKEN')
 access_token_secret = os.getenv('TWITTER_ACCESS_TOKEN_SECRET')
 bearer_token = os.getenv('TWITTER_ACCESS_TOKEN_SECRET')
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
 
 def check_official(event, context):
+    logger.info("Authentication OK.")
+    # LineBotAPIオブジェクトを作成する
+    token = os.getenv('LINE_ACCESS_TOKEN')
+    line_bot_api = LineBotApi(token)
+
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth)
 
     tweets = api.user_timeline(screen_name='hiratahirata14')
+    official_tweets = []
     for t in tweets:
         if not re.match('^RT', t.text) and t.created_at >= datetime.now() - timedelta(hours=1, minutes=1):
             print(datetime.now())
-            print(t.created_at)
-        else:
             print(t.text)
+        else:
+            print(t.user.profile_image_url_https)
+            official_tweets.append(
+                {
+                    'text': t.text,
+                    'profile_image': t.user.profile_image_url_https.replace('_normal', ''),
+                    'tweet_url': 'https://twitter.com/hiratahirata14/status/' + t.id_str,
+                }
+            )
             print('---------------')
+
+    # print(official_tweets)
+    profile_image = api.get_user(screen_name='hiratahirata14')
+    print(profile_image)
+    # for o in official_tweets:
+    bubble = BubbleContainer(
+        type="bubble",
+        direction='ltr',
+        hero=ImageComponent(
+            url=official_tweets[0]['profile_image'],
+            size='full',
+            aspect_ratio='10:10',
+            aspect_mode='cover',
+            action=URIAction(
+                  uri=official_tweets[0]['tweet_url'], label='label')
+        ),
+        body=BoxComponent(
+            type="box",
+            layout='vertical',
+            contents=[
+                # title
+                TextComponent(
+                    type="text",
+                    text=official_tweets[0]['text'],
+                    weight='bold',
+                    size='sm',
+                ),
+            ]
+        ),
+    )
+    message = FlexSendMessage(alt_text="オフィシャルのお知らせ", contents=bubble)
+    user_id = os.getenv('USER_ID')
+    line_bot_api.push_message(
+        user_id,
+        message
+    )
 
 
 if __name__ == "__main__":
