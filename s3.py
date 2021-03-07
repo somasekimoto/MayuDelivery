f72650175e0384daffb51c050acdae2d95b6bde6
SomaSekimoto <0421.soma@gmail.com>
2021-03-07 11:34:40 +0900

    fix: dir name to 'tmp'

--- s3.py
+++ s3.py
@@ -7,10 +7,7 @@ import shutil
 
 
 def upload(file, index):
-  TEMP_DIR = "temp_media"
- 
-  if not os.path.exists(TEMP_DIR):
-      os.makedirs(TEMP_DIR)
+  TEMP_DIR = "tmp"
   
   s3 = boto3.client('s3')
   bucket_name = os.getenv('S3_BUCKET')
@@ -22,14 +19,13 @@ def upload(file, index):
 
   with urllib.request.urlopen(file, timeout=5) as data:
     img = data.read()
-    local_file = './'+ TEMP_DIR +'/' + str(index) + ext
+    local_file = '/'+ TEMP_DIR +'/' + str(index) + ext
     with open(local_file, 'wb') as f:
       f.write(img)
 
   today_str = datetime.today().strftime(f"%Y-%m-%d")
   s3_object_path = today_str + '/dailyMayu' + str(index) + ext
   s3.upload_file(local_file, bucket_name, s3_object_path)
-  shutil.rmtree(TEMP_DIR)
 
 
 
