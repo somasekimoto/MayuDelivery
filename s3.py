46d057432217b1d3b3facc16b3222d24f0f7f4a2
SomaSekimoto <0421.soma@gmail.com>
2022-01-30 00:12:51 +0900

    success: list s3 files

--- s3.py
+++ s3.py
@@ -4,33 +4,42 @@ import time
 import urllib
 import os.path
 import shutil
+import json
 
 
 def upload(file, index):
-  TEMP_DIR = "tmp"
-  
-  s3 = boto3.client('s3')
-  bucket_name = os.getenv('S3_BUCKET')
+    TEMP_DIR = "tmp"
 
-  root, ext = os.path.splitext(file)
-  qs = urllib.parse.urlparse(file).query
-  if len(qs):
-    ext = ext.replace('?' + qs, '')
+    s3 = boto3.client('s3')
+    bucket_name = os.getenv('S3_BUCKET')
 
-  with urllib.request.urlopen(file, timeout=5) as data:
-    img = data.read()
-    local_file = '/'+ TEMP_DIR +'/' + str(index) + ext
-    with open(local_file, 'wb') as f:
-      f.write(img)
+    root, ext = os.path.splitext(file)
+    qs = urllib.parse.urlparse(file).query
+    if len(qs):
+        ext = ext.replace('?' + qs, '')
 
-  today_str = datetime.today().strftime(f"%Y-%m-%d")
-  s3_object_path = today_str + '/dailyMayu' + str(index) + ext
-  s3.upload_file(local_file, bucket_name, s3_object_path)
+    with urllib.request.urlopen(file, timeout=5) as data:
+        img = data.read()
+        local_file = '/' + TEMP_DIR + '/' + str(index) + ext
+        with open(local_file, 'wb') as f:
+            f.write(img)
+
+    today_str = datetime.today().strftime(f"%Y-%m-%d")
+    s3_object_path = today_str + '/dailyMayu' + str(index) + ext
+    s3.upload_file(local_file, bucket_name, s3_object_path)
 
 
+def all_files(event, context):
+    s3 = boto3.resource('s3')
+    bucket_name = os.getenv('S3_BUCKET', 'stg-mayu-media-daily')
+    bucket = s3.Bucket(bucket_name)
+    keys = [obj.key for obj in bucket.objects.all()]
+    response = {
+        "body": json.dumps({"keys": keys}),
+    }
+    return response
 
 
 if __name__ == '__main__':
-    upload(None, None)
-
-
+    # upload(None, None)
+    print(all_files(None, None))
